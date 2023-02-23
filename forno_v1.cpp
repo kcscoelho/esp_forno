65302aeac10f080f9b9c49cb8e792606339723d3
Kleber C S Coelho <kcsc29@gmail.com>
2023-02-23 16:48:34 -0300

    versão 2
    Removido LCD, direcionado saida para serial

--- forno_v1.cpp
+++ forno_v1.cpp
@@ -38,12 +38,17 @@ Então digitar o nome da biblioteca e clicar em Install.
 Dica: Para acessar a tela do gerenciador de bibliotecas, com a interface do Arduino IDE aberta, utilize o atalho Ctrl + Shift + i
 
 
+
+versão 1: inicial.
+versão 2: removido LCD, direcionado informações para serial.
+
+
 */
 #include <Wire.h>
 #include <SPI.h>
 #include <max6675.h>
 #include <Adafruit_GFX.h>
-#include <Adafruit_SSD1306.h>
+//#include <Adafruit_SSD1306.h>
 #include <Adafruit_Sensor.h>
 #include <Adafruit_BME680.h>
 
@@ -65,17 +70,17 @@ Adafruit_BME680 bme; // I2C
 //Adafruit_BME680 bme(BME_CS); // hardware SPI
 //Adafruit_BME680 bme(BME_CS, BME_MOSI, BME_MISO,  BME_SCK);
 
-#define SCREEN_WIDTH 128 // OLED display width, in pixels
-#define SCREEN_HEIGHT 64 // OLED display height, in pixels
+//#define SCREEN_WIDTH 128 // OLED display width, in pixels
+//#define SCREEN_HEIGHT 64 // OLED display height, in pixels
 
 // Declaration for an SSD1306 display connected to I2C (SDA, SCL pins)
 // The pins for I2C are defined by the Wire-library. 
 // On an arduino UNO:       A4(SDA), A5(SCL)
 // On an arduino MEGA 2560: 20(SDA), 21(SCL)
 // On an arduino LEONARDO:   2(SDA),  3(SCL), ...
-#define OLED_RESET     -1 // Reset pin # (or -1 if sharing Arduino reset pin)
-#define SCREEN_ADDRESS 0x3C ///< See datasheet for Address; 0x3D for 128x64, 0x3C for 128x32
-Adafruit_SSD1306 display(SCREEN_WIDTH, SCREEN_HEIGHT, &Wire, OLED_RESET);
+//#define OLED_RESET     -1 // Reset pin # (or -1 if sharing Arduino reset pin)
+//#define SCREEN_ADDRESS 0x3C ///< See datasheet for Address; 0x3D for 128x64, 0x3C for 128x32
+//Adafruit_SSD1306 display(SCREEN_WIDTH, SCREEN_HEIGHT, &Wire, OLED_RESET);
 //Adafruit_SSD1306 display = Adafruit_SSD1306(128, 32, &Wire);
 //Adafruit_SSD1306 display = Adafruit_SSD1306(128, 64, &Wire, -1);
 
@@ -85,6 +90,7 @@ void setup() {
   Serial.begin(115200);
 
 // DISPLAY
+/*
   display.begin(SSD1306_SWITCHCAPVCC, SCREEN_ADDRESS);  
 if(!display.begin(SSD1306_SWITCHCAPVCC, SCREEN_ADDRESS)) { 
   Serial.println(F("SSD1306 allocation failed"));
@@ -95,6 +101,7 @@ delay(100);
 display.clearDisplay();
 display.setTextSize(1);
 display.setTextColor(WHITE);
+*/
 
 // PRESSAO
   if (!bme.begin()) {
@@ -112,17 +119,17 @@ display.setTextColor(WHITE);
 
 
 void loop() {
-  display.setCursor(0,0);
-  display.clearDisplay();
+//  display.setCursor(0,0);
+//  display.clearDisplay();
 
 // TEMPO PAR
   float temperature = module.readCelsius(); 
-  /*
+  
   Serial.print("Temperature: ");
   Serial.print(temperature);
   Serial.println(F("°C "));   
-  */
-
+  
+/*
   display.setTextSize(1);
   display.setCursor(0,10);
   display.print("Temperatura: ");
@@ -138,6 +145,7 @@ void loop() {
   display.display();
   delay(3000);
   display.clearDisplay();
+*/
 
 // PRESSAO
   if (! bme.performReading()) {
@@ -147,9 +155,9 @@ void loop() {
   /*
   Serial.print("Temperature = "); Serial.print(bme.temperature); Serial.println(" *C");
   display.print("Temperature: "); display.print(bme.temperature); display.println(" *C");
-
+*/
   Serial.print("Pressure = "); Serial.print(bme.pressure / 100.0); Serial.println(" hPa");
-  display.print("Pressure: "); display.print(bme.pressure / 100); display.println(" hPa");
+/*display.print("Pressure: "); display.print(bme.pressure / 100); display.println(" hPa");
 
   Serial.print("Humidity = "); Serial.print(bme.humidity); Serial.println(" %");
   display.print("Humidity: "); display.print(bme.humidity); display.println(" %");
@@ -158,10 +166,10 @@ void loop() {
   display.print("Gas: "); display.print(bme.gas_resistance / 1000.0); display.println(" KOhms");
   */
   
-  display.print("Pressão: "); display.print(bme.pressure / 100); display.println(" hPa");
+  //display.print("Pressão: "); display.print(bme.pressure / 100); display.println(" hPa");
   
-  display.display();
-  delay(3000);
-  display.clearDisplay();
+  //display.display();
+  //delay(3000);
+  //display.clearDisplay();
 
 }
